357b27d7fb386dc6c5902a44b1285e3a1e2c18c5
coderkartikeya <kartikeyavats04@gmail.com>
2024-03-29 17:16:58 +0530

    corrected

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,52 +1,42 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useGlobalContext } from '../context/user';
 import Profile_1 from '../components/Profile_1';
 import Side_nav from '../components/Side_nav';
 import pf from '../styles/profile_1.module.css';
 
-
 const Page = () => {
-    const [info ,getInfo]=useState();
-    const{userId}=useGlobalContext();
+    const [info, setInfo] = useState(null); // Initialize info as null
 
-    
+    const { userId } = useGlobalContext();
+    const q = JSON.stringify({ username: userId });
 
-    const q=JSON.stringify({
-        username:userId
-    })
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const req = await fetch('/api/info', {
+                    method: 'POST',
+                    body: q,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                const ans = await req.json();
+                setInfo(ans.body);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
-    useEffect(()=>{
-        const data=async()=>{
-            const req = await fetch('/api/info', {
-                method: 'POST',
-                body: q,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            const ans=await req.json();
-            console.log(ans);
-            getInfo(ans.body);
+        fetchData();
+    }, [userId, q]);
 
-        }
-        data();
-    },[userId,q]
-    )
+    return (
+        <div className={pf.main}>
+            <Side_nav />
+            {info ? <Profile_1 na_={info.username} pi_c={info.profile} /> : <p>Loading...</p>}
+        </div>
+    );
+};
 
-    
-
-
-    
-
-    
-
-  return (
-    <div className={pf.main}>
-        <Side_nav/>
-        <Profile_1 na_={info.username} pi_c={info.profile}  />
-    </div>
-  )
-}
-
-export default Page
+export default Page;
